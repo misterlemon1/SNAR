a51d7dcb18339d22e830f1c47cdaf8de260b9f79
Daria <darinamya@gmail.com>
2026-06-13 04:46:14 +0300

    Файлы

--- HW6/Teplii_Stan - Yasenevo.py
+++ HW6/Teplii_Stan - Yasenevo.py
@@ -1,7 +1,7 @@
 import FUNCTION as fct
 
-directory= "Teplii_Stan - Yasenevo - Novoyasenevo/2026-06-11_18-56-46.727_Active 10 Pro"
-SESSION_LABEL = "Teplii_Stan - Yasenevo - Novoyasenevo"           # Заголовок графика; если пусто — берётся из метаданных
+directory= "Teplii_Stan - Yasenevo (Obman №2) - Novoyasenevo/2026-06-11_18-56-46.727_Active 10 Pro"
+SESSION_LABEL = "Teplii_Stan - Yasenevo (Obman №2) - Novoyasenevo"           # Заголовок графика; если пусто — берётся из метаданных
 SAVE_PATH     = ""           # Путь для сохранения PNG; если пусто — показывается окно
 
 fct.Processing(directory,SESSION_LABEL,SAVE_PATH)
